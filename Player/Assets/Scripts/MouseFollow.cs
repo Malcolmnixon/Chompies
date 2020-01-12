e0233ca7861601aa5b78474d90a9fa7268e4daed
Samuel Sarette <samuelsarette@linux.com>
2020-01-12 17:15:57 -0500

    add mouse follow

---
+++ Player/Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class MouseFollow : MonoBehaviour {
+ 
+    public float speed = 6.0f;
+  
+    void Start()
+    {
+    }
+ 
+    private void Update()
+    {
+        if (Input.GetMouseButton (0)) {
+
+            var halfWidth = (Screen.width / 2);
+            var halfHeight = (Screen.height / 2);
+
+            var directionX = Input.mousePosition.x < halfWidth ? -1 : 1;
+            var directionY = Input.mousePosition.y < halfHeight ? -1 : 1;
+
+            var amountX = (Input.mousePosition.x - halfWidth) / halfWidth;
+            var amountY = (Input.mousePosition.y - halfHeight) / halfHeight;
+                       
+            var newX = amountX * speed * Time.deltaTime;
+            var newZ = amountY * speed * Time.deltaTime;
+
+            Debug.Log($"New {newX}, {newZ}");
+
+            transform.Translate(new Vector3 (newX, 0, newZ));
+        }
+    }
+}
+ 
