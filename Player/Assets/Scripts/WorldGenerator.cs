9f46c450b1f60b3bfef2d69c0830f2a70c018803
Samuel Sarette <samuelsarette@linux.com>
2020-01-12 17:46:22 -0500

    Added a world generator object to the scene

---
+++ Player/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+
+    [Header("Settings")]
+
+    [Range(-5f, 5f)]
+    [SerializeField]
+    private float randomness;
+
+
+    [Header("Game Objects")]
+
+    [Tooltip("Base of the world, will be reconstructed")]
+    [SerializeField]
+    public GameObject world;
+
+    
+    [Header("Prefabs")]
+    
+    [SerializeField]
+    public GameObject heroPrefab;
+    
+    [Space]
+
+    [SerializeField]
+    public GameObject hordePrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
