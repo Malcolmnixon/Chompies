bb19ff17662fc8bb428e31b2382e676f532ad0ee
Samuel Sarette <samuelsarette@linux.com>
2020-01-12 17:16:12 -0500

    update build of shared

--- Shared/build.sh
+++ Shared/build.sh
@@ -3,3 +3,5 @@
 dotnet restore
 dotnet build Chompies.Shared.sln --configuration Release
 dotnet test Chompies.Shared.Tests/Chompies.Shared.Tests.csproj
+dotnet publish Chompies.Shared.sln --configuration Release
+cp Chompies.Shared/bin/Release/netstandard2.0/publish/*.dll ../Player/Assets/Libraries
